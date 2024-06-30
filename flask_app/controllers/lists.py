4d629b8574b4a3b6a6d4fb84884002281d269bdc
ColoBabbo <navessolutions@gmail.com>
2024-06-29 20:04:57 -0600

    Created Show List functionality

--- flask_app/controllers/lists.py
+++ flask_app/controllers/lists.py
@@ -26,3 +26,23 @@ def add_list() -> None:
     else:
         flash('Please Login', 'login')
     return redirect('/dashboard')
+
+@app.get('/trip/<int:trip_id>/list/<int:list_id>')
+def show_one_list(trip_id:int, list_id:int):
+    if session.get('logged_in'):
+        this_trip = trip.Trip.get_one(trip_id)
+        if this_trip == False:
+            flash('No such record!', 'unauthorized')
+            return redirect(url_for('show_all_trips'))
+        if session.get('list_attempt'):
+            pre_fill = {
+                'list_name': session['list_attempt']['list_name'],
+            }
+        else:
+            pre_fill = {
+                'list_name': '',
+            }
+        return render_template('show_one_list.html', this_trip = this_trip, pre_fill = pre_fill, list_id = list_id)
+    else:
+        flash('Please Login', 'login')
+    return redirect('/')
